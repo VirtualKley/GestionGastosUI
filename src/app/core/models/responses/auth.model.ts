0531aa469d70440ad98bf15d5a73a795ff95469f
VirtualKley <javolkley@gmail.com>
2026-05-17 08:49:46 -0500

    Se agrega las interfaces de los modelos y se ve la implementacion de index (barrels) para una importacion limpia

---
+++ src/app/core/models/responses/auth.model.ts
@@ -0,0 +1,6 @@
+export interface AuthResponse{
+    token: string;
+    nombre: string;
+    email: string;
+    rol: string;
+}
---
+++ src/app/core/models/responses/usuario.model.ts
@@ -0,0 +1,8 @@
+export interface Usuario{
+    id: number;
+    nombre: string;
+    email: string;
+    rol: number;
+    activo: boolean;
+    fechaCreacion: string;
+}
