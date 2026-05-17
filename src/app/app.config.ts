9f4e0d6f3d0691eb532a5ec66bd53f7788efd165
VirtualKley <javolkley@gmail.com>
2026-05-17 08:11:27 -0500

    Configuraciones de PrimeNG, iconos y cosas iniciales

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection(
+      { eventCoalescing: true }),
+      provideRouter(routes), //Registra el sistema de rutas de Angular
+      provideAnimations(),
+      provideHttpClient(withInterceptorsFromDi()) //provideHttpClient registra el cliente HTTP para poder realizar peticiones, withINterceptorFromDi() habilita los interceptores que registres en el contenedor de dependencias
+    ]
 };
